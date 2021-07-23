166a84547dca48bb1c5c902ec5ea14fdcddadd4c
Innei <tukon479@gmail.com>
2021-07-23 11:54:10 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
+import { fastifyApp } from './core/adapt/fastify'
+import { isDev } from './utils'
+import { Logger } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+const PORT = parseInt(process.env.PORT) || 2333
+const APIVersion = 1
+const Origin = process.env.ORIGIN || ''
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(2333);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastifyApp,
+  )
+
+  const hosts = Origin.split(',').map((host) => new RegExp(host, 'i'))
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allow = hosts.some((host) => host.test(origin))
+
+      callback(null, allow)
+    },
+    credentials: true,
+  })
+
+  app.setGlobalPrefix(isDev ? '' : `api/v${APIVersion}`)
+  if (isDev) {
+    const options = new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('The blog API description')
+      .setVersion(`${APIVersion}`)
+      .addSecurity('bearer', {
+        type: 'http',
+        scheme: 'bearer',
+      })
+      .addBearerAuth()
+      .build()
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('api-docs', app, document)
+  }
+
+  await app.listen(PORT, '0.0.0.0', () => {
+    if (isDev) {
+      Logger.debug(`http://localhost:${PORT}/api-docs`)
+    }
+    Logger.log('Server is up.')
+  })
 }
-bootstrap();
+bootstrap()
