212f9e48ad07d1bc4aa4abd845c55b8210bbaceb
Innei <tukon479@gmail.com>
2021-07-29 19:37:55 +0800

    feat: docker init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { InitModule } from './modules/init/init.module'
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [InitModule],
+  imports: [InitModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
