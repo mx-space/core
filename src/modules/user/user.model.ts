4d147da863bd8774a06a201681546615b4133594
Innei <tukon479@gmail.com>
2021-07-29 19:49:48 +0800

    feat: model init

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,77 @@
+import { DocumentType, modelOptions, prop } from '@typegoose/typegoose'
+import { Schema } from 'mongoose'
+import { BaseModel } from 'src/shared/base.module'
+import { hashSync } from 'bcrypt'
+export type UserDocument = DocumentType<UserModel>
+
+export class OAuthModel {
+  @prop()
+  platform: string
+  @prop()
+  id: string
+}
+
+export class TokenModel {
+  @prop()
+  created: Date
+
+  @prop()
+  token: string
+
+  @prop()
+  expired?: Date
+
+  @prop({ unique: true })
+  name: string
+}
+
+@modelOptions({ options: { customName: 'User' } })
+export class UserModel extends BaseModel {
+  @prop({ required: true, unique: true, trim: true })
+  username!: string
+
+  @prop({ trim: true })
+  name!: string
+
+  @prop()
+  introduce?: string
+
+  @prop()
+  avatar?: string
+
+  @prop({
+    select: false,
+    get(val) {
+      return val
+    },
+    set(val) {
+      return hashSync(val, 6)
+    },
+    required: true,
+  })
+  password!: string
+
+  @prop()
+  mail?: string
+
+  @prop()
+  url?: string
+
+  @prop()
+  lastLoginTime?: Date
+
+  @prop({ select: false })
+  lastLoginIp?: string
+
+  @prop({ type: Schema.Types.Mixed })
+  socialIds?: any
+
+  @prop({ select: true, required: true })
+  authCode!: string
+
+  @prop({ type: TokenModel, select: false })
+  apiToken?: TokenModel[]
+
+  @prop({ type: OAuthModel, select: false })
+  oauth2?: OAuthModel[]
+}
---
+++ src/shared/base.module.ts
@@ -0,0 +1,50 @@
+import { prop } from '@typegoose/typegoose'
+import { IsNotEmpty, IsString } from 'class-validator'
+export class BaseModel {
+  created?: Date
+}
+
+@modelOptions({
+  schemaOptions: { _id: false },
+})
+class Image {
+  @prop()
+  width?: number
+
+  @prop()
+  height?: number
+
+  @prop()
+  accent?: string
+
+  @prop()
+  type?: string
+
+  @prop()
+  src: string
+}
+
+export abstract class BaseCommentIndexModel extends BaseModel {
+  @prop({ default: 0 })
+  commentsIndex?: number
+
+  @prop({ default: true })
+  allowComment: boolean
+}
+
+export abstract class WriteBaseModel extends BaseCommentIndexModel {
+  @prop({ trim: true, index: true, required: true })
+  @IsString()
+  @IsNotEmpty()
+  title: string
+
+  @prop({ trim: true })
+  @IsString()
+  text: string
+
+  @prop({ type: Image })
+  images?: Image[]
+
+  @prop({ default: () => new Date() })
+  modified: Date
+}
