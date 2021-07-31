d13829bcaaade0b7b61b9b3ba930892f1fa8f1b3
Innei <tukon479@gmail.com>
2021-07-31 16:30:52 +0800

    chore: format

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { InitModule } from './modules/init/init.module'
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules/user/user.module'
 
 @Module({
   imports: [InitModule, UserModule],
