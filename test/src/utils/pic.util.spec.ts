e718dd0d24d71cee91543980e8d918fc98484895
Innei <tukon479@gmail.com>
2021-09-13 18:37:02 +0800

    chore: add sourcemap

--- test/src/utils/pic.util.spec.ts
+++ test/src/utils/pic.util.spec.ts
@@ -1,12 +1,16 @@
+import { sleep } from '~/utils/index.util'
 import { pickImagesFromMarkdown } from '~/utils/pic.util'
 
 describe('src/utils/pic.util', () => {
-  it('test marked ast', () => {
+  it('test marked ast', async () => {
     const res = pickImagesFromMarkdown(`
 ![](https://cdn.innei.ren/bed/2021/0813211729.jpeg)
 
 ![](https://cdn.innei.ren/bed/2021/0813212633.jpg)
 `)
+    // FIXME: ReferenceError: You are trying to import a file after the Jest environment has been torn down
+    // gifwrap@0.9.2
+    await sleep(1)
     expect(res).toEqual([
       'https://cdn.innei.ren/bed/2021/0813211729.jpeg',
       'https://cdn.innei.ren/bed/2021/0813212633.jpg',
