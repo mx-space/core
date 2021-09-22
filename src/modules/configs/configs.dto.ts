a86ebf9d9f370fd177239750245ae2da09f40a74
Innei <tukon479@gmail.com>
2021-09-22 14:41:20 +0800

    fix: unqi ip and spam keyword

--- src/modules/configs/configs.dto.ts
+++ src/modules/configs/configs.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Transform, Type } from 'class-transformer'
 import {
+  ArrayUnique,
   IsBoolean,
   IsEmail,
   IsInt,
+  IsIP,
   IsNotEmpty,
   IsOptional,
   IsString,
@@ -87,9 +89,11 @@ export class CommentOptionsDto {
 
   @IsString({ each: true })
   @IsOptional()
+  @ArrayUnique()
   spamKeywords?: string[]
 
-  @IsString({ each: true })
+  @IsIP(undefined, { each: true })
+  @ArrayUnique()
   @IsOptional()
   blockIps?: string[]
   @IsOptional()
