c4cf9b3eb00d044dccaa260d015ef1c86c04d588
Innei <tukon479@gmail.com>
2021-10-01 18:58:21 +0800

    fix: remove unnessary app info field

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -25,23 +25,12 @@ export class AppController {
   ) {}
   @Get(['/', '/info'])
   async appInfo() {
-    let hash = ''
-    try {
-      await $`git log --pretty=oneline | head -n 1 | cut -d' ' -f1 | cat`
-    } catch (e: any) {
-      // HACK https://www.codenong.com/19120263/
-      if (e.exitCode == 141) {
-        hash = e.stdout.trim()
-      }
-    }
-
     return {
       name: PKG.name,
       author: PKG.author,
       version: PKG.version,
       homepage: PKG.homepage,
       issues: PKG.issues,
-      hash,
     }
   }
 
