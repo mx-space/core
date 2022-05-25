2c237b7a8dc2032ac3926f736574b11008c8249d
Innei <tukon479@gmail.com>
2022-05-25 21:20:30 +0800

    fix: demo cache key prefix

--- src/utils/redis.util.ts
+++ src/utils/redis.util.ts
@@ -1,10 +1,14 @@
+import { isInDemoMode } from '~/app.config'
 import { RedisKeys } from '~/constants/cache.constant'
+
+type Prefix = 'mx' | 'mx-demo'
+const prefix = isInDemoMode ? 'mx-demo' : 'mx'
 
 export const getRedisKey = <T extends string = RedisKeys | '*'>(
   key: T,
   ...concatKeys: string[]
-): `mx:${T}${string | ''}` => {
-  return `mx:${key}${
+): `${Prefix}:${T}${string | ''}` => {
+  return `${prefix}:${key}${
     concatKeys && concatKeys.length ? `:${concatKeys.join('_')}` : ''
   }`
 }
