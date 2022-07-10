8925e526171e289324ab8cd77c94c8713e632c30
Innei <tukon479@gmail.com>
2022-07-10 14:06:49 +0800

    feat: add repl
    Signed-off-by: Innei <tukon479@gmail.com>

---
+++ src/repl.ts
@@ -0,0 +1,10 @@
+import { repl } from '@nestjs/core'
+
+import { register } from './global/index.global'
+
+async function bootstrap() {
+  register()
+  const { AppModule } = await import('./app.module')
+  await repl(AppModule)
+}
+bootstrap()
