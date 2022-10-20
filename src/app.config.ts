380a88a1bf2594a9e69b990fa849c4bca39e7aa8
Innei <tukon479@gmail.com>
2022-10-20 22:24:52 +0800

    feat: support load yaml config
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,9 +1,56 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import type { AxiosRequestConfig } from 'axios'
 import cluster from 'cluster'
-import { argv } from 'zx-cjs'
+import { program } from 'commander'
+import { readFileSync } from 'fs'
+import { load as yamlLoad } from 'js-yaml'
+import path from 'path'
 
 import { cwd, isDev, isMainCluster, isTest } from './global/env.global'
+
+const commander = program
+  .option('-p, --port <number>', 'server port')
+  .option('--demo', 'enable demo mode')
+  .option(
+    '--allowed_origins <string>',
+    'allowed origins, e.g. innei.ren,*.innei.ren',
+  )
+  .option('-c, --config <path>', 'load yaml config from file')
+
+  // db
+  .option('--collection_name <string>', 'mongodb collection name')
+  .option('--db_host <string>', 'mongodb database host')
+  .option('--db_port <number>', 'mongodb database port')
+  .option('--db_user <string>', 'mongodb database user')
+  .option('--db_password <string>', 'mongodb database password')
+
+  // redis
+  .option('--redis_host <string>', 'redis host')
+  .option('--redis_port <number>', 'redis port')
+  .option('--redis_password <string>', 'redis password')
+  .option('--disable_cache', 'disable redis cache')
+
+  // jwt
+  .option('--jwt_secret <string>', 'custom jwt secret')
+  .option('--jwt_expire <number>', 'custom jwt expire time(d)')
+
+  // cluster
+  .option('--cluster', 'enable cluster mode')
+  .option('--cluster_workers <number>', 'cluster worker count')
+
+  // debug
+  .option('--http_request_verbose', 'enable http request verbose')
+
+commander.parse()
+
+const argv = commander.opts()
+
+if (argv.config) {
+  const config = yamlLoad(
+    readFileSync(path.join(String(process.cwd()), argv.config), 'utf8'),
+  )
+  Object.assign(argv, config)
+}
 
 export const PORT = argv.port || process.env.PORT || 2333
 export const API_VERSION = 2
@@ -36,8 +83,8 @@ export const MONGO_DB = {
   user: argv.db_user || '',
   password: argv.db_password || '',
   get uri() {
-    let userPassword =
-      this.user && this.password ? this.user + ':' + this.password + '@' : ''
+    const userPassword =
+      this.user && this.password ? `${this.user}:${this.password}@` : ''
     return `mongodb://${userPassword}${this.host}:${this.port}/${
       isTest ? 'mx-space_unitest' : this.dbName
     }`
