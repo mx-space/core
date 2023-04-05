710c083c4f5352dfb3c7829cb992533cd205658d
Innei <tukon479@gmail.com>
2023-04-05 20:27:15 +0800

    feat: add snippet for xlog summary
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/modules/serverless/pack/xlog/index.ts
+++ src/modules/serverless/pack/xlog/index.ts
@@ -1,3 +1,4 @@
 import get_page_id from './get_page_id'
+import get_summary from './get_summary'
 
-export const xLogPackSnippets = [get_page_id]
+export const xLogPackSnippets = [get_page_id, get_summary]
