80d24e81ac1ddc08ecc8c76f55dd3d319c9bd9a6
Innei <tukon479@gmail.com>
2023-06-09 00:41:05 +0800

    test
    Signed-off-by: Innei <tukon479@gmail.com>

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+command="node index.js --redis_host=redis --db_host=mongo --allowed_origins=${ALLOWED_ORIGINS} --jwt_secret=${JWT_SECRET} --color"
+
+if [ -n "$ENCRYPT_KEY" ]; then
+  command+=" --encrypt_key=${ENCRYPT_KEY}"
+fi
+
+if [ -n "$ENCRYPT_ENABLE" ]; then
+  command+=" --encrypt_enable=${ENCRYPT_ENABLE}"
+fi
+
+exec $command
