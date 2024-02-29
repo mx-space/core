a142e8b2011c1e097cefd76f92f11d1a3242d183
Innei <i@innei.in>
2024-02-29 20:16:06 +0800

    fix: log
    Signed-off-by: Innei <i@innei.in>

--- apps/core/src/global/consola.global.ts
+++ apps/core/src/global/consola.global.ts
@@ -11,6 +11,7 @@ import { isTest } from './env.global'
 const logger = createLogger({
   writeToFile: {
     loggerDir: LOG_DIR,
+    errWriteToStdout: true,
   },
 })
 Logger.setLoggerInstance(logger)
