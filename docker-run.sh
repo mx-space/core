e08ddd35d55ef5998e3120949a56ff784d369470
Innei <i@innei.in>
2024-03-11 13:25:10 +0800

    fix: docker run script
    Signed-off-by: Innei <i@innei.in>

--- docker-run.sh
+++ docker-run.sh
@@ -4,9 +4,15 @@ command="node index.js --redis_host=redis --db_host=mongo \
   --allowed_origins=${ALLOWED_ORIGINS} \
   --jwt_secret=${JWT_SECRET} \
   --color \
-  --http_cache_enable_cdn_header=${CDN_CACHE_HEADER} \
-  --http_cache_enable_force_cache_header=${FORCE_CACHE_HEADER} \
   "
+
+if [ -n "$CDN_CACHE_HEADER" ]; then
+  command+=" --http_cache_enable_cdn_header"
+fi
+
+if [ -n "$FORCE_CACHE_HEADER" ]; then
+  command+=" --http_cache_enable_force_cache_header"
+fi
 
 if [ -n "$ENCRYPT_KEY" ]; then
   command+=" --encrypt_key=${ENCRYPT_KEY}"
