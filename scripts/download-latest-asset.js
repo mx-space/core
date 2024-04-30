ee92d486cad2ee4b65008c31c8495ec5f9f52eeb
Innei <i@innei.in>
2024-04-30 15:57:38 +0800

    fix: download asset script

--- scripts/download-latest-asset.js
+++ scripts/download-latest-asset.js
@@ -1,27 +1,22 @@
-#!/usr/bin/env zx
-/* eslint-disable */
-// @ts-check
-
 function getOsBuildAssetName() {
   const platform = process.platform
   const kernelMap = {
     darwin: 'macos',
-    linux: 'ubuntu',
+    linux: 'linux',
     win32: 'windows',
   }
   const os = kernelMap[platform]
   if (!os) {
     throw new Error('No current platform build. Please build manually')
   }
-  return `release-${os}-latest.zip`
+  return `release-${os}.zip`
 }
 
 const { appendFileSync } = require('fs')
 
-const PKG = require('../package.json')
 async function main() {
   const res = await fetch(
-    `https://api.github.com/repos/${PKG.repository.directory}/releases/latest`,
+    `https://api.github.com/repos/mx-space/core/releases/latest`,
   )
   const data = await res.json()
   const downloadUrl = data.assets.find((asset) =>
@@ -31,9 +26,7 @@ async function main() {
     throw new Error('no download url found')
   }
 
-  const buffer = await fetch('https://cc.shizuri.net/' + downloadUrl).then(
-    (res) => res.arrayBuffer(),
-  )
+  const buffer = await fetch(downloadUrl).then((res) => res.arrayBuffer())
   appendFileSync(`release-downloaded.zip`, Buffer.from(buffer))
   await $`unzip release-downloaded.zip -d mx-server`
 }
