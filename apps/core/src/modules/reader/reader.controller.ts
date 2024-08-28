4d8575570bd7af735a860f65f6473f5c4fce64bd
Innei <i@innei.in>
2024-08-28 23:38:54 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- apps/core/src/modules/reader/reader.controller.ts
+++ apps/core/src/modules/reader/reader.controller.ts
@@ -1,6 +1,27 @@
+import { Body, Get, Patch } from '@nestjs/common'
+
 import { ApiController } from '~/common/decorators/api-controller.decorator'
 import { Auth } from '~/common/decorators/auth.decorator'
+import { MongoIdDto } from '~/shared/dto/id.dto'
+
+import { ReaderService } from './reader.service'
 
 @ApiController('readers')
 @Auth()
-export class ReaderController {}
+export class ReaderController {
+  constructor(private readonly readerService: ReaderService) {}
+  @Get('/')
+  async find() {
+    return this.readerService.find()
+  }
+
+  @Patch('/as-owner')
+  async updateAsOwner(@Body() body: MongoIdDto) {
+    return this.readerService.updateAsOwner(body.id)
+  }
+
+  @Patch('/revoke-owner')
+  async revokeOwner(@Body() body: MongoIdDto) {
+    return this.readerService.revokeOwner(body.id)
+  }
+}
--- apps/core/src/modules/reader/reader.service.ts
+++ apps/core/src/modules/reader/reader.service.ts
@@ -1,5 +1,27 @@
+import { Types } from 'mongoose'
 import type { DatabaseService } from '~/processors/database/database.service'
+
+import { AUTH_JS_USER_COLLECTION } from '../auth/auth.constant'
 
 export class ReaderService {
   constructor(private readonly databaseService: DatabaseService) {}
+
+  find() {
+    return this.databaseService.db
+      .collection(AUTH_JS_USER_COLLECTION)
+      .find()
+      .toArray()
+  }
+
+  async updateAsOwner(id: string) {
+    return this.databaseService.db
+      .collection(AUTH_JS_USER_COLLECTION)
+      .updateOne({ _id: new Types.ObjectId(id) }, { $set: { isOwner: true } })
+  }
+
+  async revokeOwner(id: string) {
+    return this.databaseService.db
+      .collection(AUTH_JS_USER_COLLECTION)
+      .updateOne({ _id: new Types.ObjectId(id) }, { $set: { isOwner: false } })
+  }
 }
