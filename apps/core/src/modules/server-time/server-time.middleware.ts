34613e5dff745e6f59674e72dfc308d2ad5a3a6b
Innei <i@innei.in>
2024-09-02 21:05:15 +0800

    fix: server time cors
    Signed-off-by: Innei <i@innei.in>

--- apps/core/src/modules/server-time/server-time.middleware.ts
+++ apps/core/src/modules/server-time/server-time.middleware.ts
@@ -12,9 +12,10 @@ export async function trackResponseTimeMiddleware(
 
   res.setHeader('Content-Type', 'application/json')
   // cors
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Max-Age', '86400')
   await next()
 
