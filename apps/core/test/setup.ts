c657e9a80aa8eb49b54482c82ff70a869385386a
Innei <tukon479@gmail.com>
2025-05-06 00:52:50 +0800

    feat(ci): add custom action for MongoDB and Redis setup
    - Introduced a new GitHub Action to streamline the setup of MongoDB and Redis for testing environments.
    - Updated CI workflows to utilize the new action, replacing direct MongoDB and Redis setup steps for improved maintainability.
    - Added `mongodb-memory-server` and `redis-memory-server` as dependencies in the project.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/core/test/setup.ts
+++ apps/core/test/setup.ts
@@ -1,6 +1,5 @@
 import { mkdirSync } from 'node:fs'
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import { chalk } from 'zx'
 
 import { Logger } from '@nestjs/common'
 
@@ -15,15 +14,10 @@ import {
 
 export async function setup() {
   mkdirSync(DATA_DIR, { recursive: true })
-  Logger.log(chalk.blue(`数据目录已经建好：${DATA_DIR}`))
   mkdirSync(TEMP_DIR, { recursive: true })
-  Logger.log(chalk.blue(`临时目录已经建好：${TEMP_DIR}`))
   mkdirSync(LOG_DIR, { recursive: true })
-  Logger.log(chalk.blue(`日志目录已经建好：${LOG_DIR}`))
   mkdirSync(USER_ASSET_DIR, { recursive: true })
-  Logger.log(chalk.blue(`资源目录已经建好：${USER_ASSET_DIR}`))
   mkdirSync(STATIC_FILE_DIR, { recursive: true })
-  Logger.log(chalk.blue(`文件存放目录已经建好：${STATIC_FILE_DIR}`))
   mkdirSync(THEME_DIR, { recursive: true })
   const db = await MongoMemoryServer.create()
   await db.stop()
