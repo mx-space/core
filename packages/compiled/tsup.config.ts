67c67ccdaf09d50d78e20820b2af2482dd603f02
Teror Fox <i@trfox.top>
2025-06-28 15:17:30 +0800

    build(tsup): 添加 import.meta.url 支持
    - 在 tsup 配置中添加 platform: 'node' 设置
    - 注入 import.meta.url 兼容层，以支持不同模块格式

--- packages/compiled/tsup.config.ts
+++ packages/compiled/tsup.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   dts: true,
   external: ['mongodb'],
   format: ['cjs'],
+  platform: 'node',
+  banner: {
+    js: `const __injected_import_meta_url = require("url").pathToFileURL(__filename).href;`,
+  },
+  esbuildOptions(options) {
+    options.define = {
+      ...options.define,
+      'import.meta.url': '__injected_import_meta_url',
+    }
+  },
 })
