800c0cb5f3924b91ce02c0e29f2c3ec47f1addba
Innei <tukon479@gmail.com>
2026-01-13 00:36:08 +0800

    fix: build mod script

--- packages/api-client/mod-dts.mjs
+++ packages/api-client/mod-dts.mjs
@@ -4,15 +4,18 @@ import path from 'node:path'
 const __dirname = new URL(import.meta.url).pathname.replace(/\/[^/]*$/, '')
 const PKG = JSON.parse(readFileSync(path.resolve(__dirname, './package.json')))
 
-const dts = path.resolve(__dirname, './dist/index.d.ts')
+const dts = path.resolve(__dirname, './dist/index.d.cts')
+const dtsm = path.resolve(__dirname, './dist/index.d.mts')
 const content = readFileSync(dts, 'utf-8')
 
-// replace declare module '../core/client'
-// with declare module '@mx-space/api-client'
-writeFileSync(
-  dts,
-  content.replaceAll(
-    /declare module '..\/core\/client'/g,
-    'declare module ' + `'${PKG.name}'`,
-  ),
-)
+for (const file of [dts, dtsm]) {
+  // replace declare module '../core/client'
+  // with declare module '@mx-space/api-client'
+  writeFileSync(
+    file,
+    content.replaceAll(
+      /declare module '..\/core\/client'/g,
+      'declare module ' + `'${PKG.name}'`,
+    ),
+  )
+}
